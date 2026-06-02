14187a96db409429f2360e06c225ca4e3d63b70d
BryantChi <bryantchi.work@gmail.com>
2026-06-02 19:48:04 +0800

    發版 v3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.gimy.tv"
         minSdk = 28
         targetSdk = 35
-        versionCode = 300
-        versionName = "3.0.0"
+        versionCode = 301
+        versionName = "3.0.1"
     }
 
     signingConfigs {
